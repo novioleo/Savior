0a7b708d52580a2d0968e5c1d9ab82b99fa4dddc
Tao Luo <744351893@qq.com>
2021-03-07 23:52:38 +0800

    初始化ncnn版的人脸检测的helper实现

---
+++ Utils/InferenceHelpers/NCNNHelpers/FaceDetectHelper.py
@@ -0,0 +1,21 @@
+from Utils.Exceptions import NetworkInputParameterException
+from Utils.InferenceHelpers.BaseInferenceHelper import NCNNInferenceHelper
+
+
+class FaceDetectInferenceHelper(NCNNInferenceHelper):
+
+    def __init__(self):
+        super().__init__('Face Detect')
+        from Libraries.face_detect import FaceDetectInfer
+        self.handler = FaceDetectInfer(None, None)
+        self.add_input('image', (224, 224, 3), '将原始照片进行center pad之后resize至224')
+        self.add_output('bboxes', (-1, 5), '经过NMS之后的人脸的bbox，'
+                                           '5个维度分别为left top x,left top y,right bottom x,right bottom y,score')
+
+    def infer(self, _image):
+        check_status, status_message = self.all_inputs[0].tensor_check(_image)
+        if check_status:
+            bboxes = self.handler.get_infer_result(_image)
+            return bboxes
+        else:
+            raise NetworkInputParameterException(status_message)
