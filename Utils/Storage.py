eb95a793fd6111b9b24a4750e77db9340c3975ce
Tao Luo <744351893@qq.com>
2021-03-09 23:40:49 +0800

    调整oss基类结构，并且实现dummy oss，方便进行本地单机测试

--- Utils/Storage.py
+++ Utils/Storage.py
@@ -7,7 +7,7 @@ import cv2
 import msgpack
 import msgpack_numpy as m
 
-from Utils.Exceptions import ImageFileSizeAbnormalException
+from Utils.Exceptions import ImageFileSizeAbnormalException, ObjectNotFoundOnOSS
 
 
 class CloudObjectStorage(ABC):
@@ -72,6 +72,52 @@ class CloudObjectStorage(ABC):
     def _general_numpy_object_decode(_to_decode_bytes):
         return msgpack.unpackb(_to_decode_bytes, object_hook=m.decode)
 
+    def download_image_file(self, _bucket_name, _object_path, _image_size_threshold=None):
+        return self._image_object_decode(self.download_data(_bucket_name, _object_path), _image_size_threshold)
+
+    def download_numpy_array(self, _bucket_name, _object_path):
+        return self._general_numpy_object_decode(self.download_data(_bucket_name, _object_path))
+
+    def upload_image_file(self, _bucket_name, _object_path, _image, _enable_compress=True, _quality_rate=90):
+        return self.upload_data(_bucket_name, _object_path + ('.webp' if _enable_compress else '.png'),
+                                self._image_object_encode(_image, _enable_compress, _quality_rate)
+                                )
+
+    def upload_numpy_array(self, _bucket_name, _object_path, _np_array):
+        return self.upload_data(_bucket_name, _object_path,
+                                self._general_numpy_object_encode(_np_array)
+                                )
+
+
+class DummyOSS(CloudObjectStorage):
+    """
+
+    单机测试用oss，全程存储在本地
+
+    """
+
+    def __init__(self, _endpoint, _access_key, _secret_key):
+        super().__init__(_endpoint, _access_key, _secret_key)
+        self.temp_directory_path = '/tmp/DummyOSS-temp-directory'
+        os.makedirs(self.temp_directory_path, exist_ok=True)
+
+    def create_bucket(self, _bucket_name):
+        os.makedirs(os.path.join(self.temp_directory_path, _bucket_name), exist_ok=True)
+
+    def download_data(self, _bucket_name, _object_path):
+        target_file_path = os.path.join(self.temp_directory_path, _bucket_name, _object_path)
+        if not os.path.exists(target_file_path):
+            raise ObjectNotFoundOnOSS(target_file_path + ' not found')
+        with open(target_file_path, mode='rb') as to_read:
+            return to_read.read()
+
+    def upload_data(self, _bucket_name, _object_path, _to_upload_object_bytes):
+        target_file_path = os.path.join(self.temp_directory_path, _bucket_name, _object_path)
+        os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
+        with open(target_file_path, mode='wb') as to_write:
+            to_write.write(_to_upload_object_bytes.read())
+        return _object_path
+
 
 class MinioOSS(CloudObjectStorage):
 
@@ -91,27 +137,19 @@ class MinioOSS(CloudObjectStorage):
             self.client.make_bucket(_bucket_name, location=self.region)
 
     def download_data(self, _bucket_name, _object_path):
-        return self.client.get_object(_bucket_name, _object_path).data
-
-    def download_image_file(self, _bucket_name, _object_path, _image_size_threshold=None):
-        return self._image_object_decode(self.download_data(_bucket_name, _object_path), _image_size_threshold)
-
-    def download_numpy_array(self, _bucket_name, _object_path):
-        return self._general_numpy_object_decode(self.download_data(_bucket_name, _object_path))
+        try:
+            response = self.client.get_object(_bucket_name, _object_path)
+            data_bytes = response.data
+            return data_bytes
+        except Exception as e:
+            raise ObjectNotFoundOnOSS(os.path.join(_bucket_name, _object_path) + ' not found')
+        finally:
+            response.close()
+            response.release_conn()
 
     def upload_data(self, _bucket_name, _object_path, _to_upload_object_bytes):
         return self.client.put_object(_bucket_name, _object_path, _to_upload_object_bytes, -1,
-                                      part_size=5 * 1024 * 1024)
-
-    def upload_image_file(self, _bucket_name, _object_path, _image, _enable_compress=True, _quality_rate=90):
-        return self.upload_data(_bucket_name, _object_path + ('.webp' if _enable_compress else '.png'),
-                                self._image_object_encode(_image, _enable_compress, _quality_rate)
-                                )
-
-    def upload_numpy_array(self, _bucket_name, _object_path, _np_array):
-        return self.upload_data(_bucket_name, _object_path,
-                                self._general_numpy_object_encode(_np_array)
-                                )
+                                      part_size=5 * 1024 * 1024).object_name
 
 
 if __name__ == '__main__':
@@ -121,7 +159,7 @@ if __name__ == '__main__':
     ag = ArgumentParser('OSS Test Example')
     ag.add_argument('--end_point_url', required=True, type=str, help='OSS服务器的URL')
     ag.add_argument('--end_point_port', required=True, type=str, help='OSS服务器的port')
-    ag.add_argument('--backend_type', required=True, choices=['minio', ], type=str, help='OSS服务器的类型')
+    ag.add_argument('--backend_type', required=True, choices=['minio', 'dummy'], type=str, help='OSS服务器的类型')
     ag.add_argument('-a', '--access_key', type=str, required=True, help='access key')
     ag.add_argument('-s', '--secret_key', type=str, required=True, help='secret key')
     ag.add_argument('-r', '--region', type=str, default=None, help='地域')
@@ -129,28 +167,34 @@ if __name__ == '__main__':
     ag.add_argument('-i', '--image_path', type=str, required=True, help='测试用的图像路径')
     args = ag.parse_args()
     if args.backend_type == 'minio':
-        minio_oss_operator = MinioOSS(
+        oss_operator = MinioOSS(
             f'{args.end_point_url}:{args.end_point_port}',
             args.access_key,
             args.secret_key,
             args.region
         )
-        bucket_name = args.bucket_name
-        minio_oss_operator.create_bucket(bucket_name)
-        img = cv2.imread(args.image_path)
-        resize_img = cv2.resize(img, (128, 128))
-        upload_image_path = os.path.join('test_images', 'image')
-        upload_image_path = minio_oss_operator.upload_image_file(
-            bucket_name, upload_image_path, resize_img, True, 90
-        ).object_name
-        image_numpy = minio_oss_operator.download_image_file(bucket_name, upload_image_path)
-        cv2.imwrite('resize_image.png', image_numpy)
-        test_numpy_array = [
-            np.random.randint(3, 5, (3, 3)).astype(np.uint8),
-            np.random.uniform(0, 1, (5, 5))
-        ]
-        upload_test_numpy_path = os.path.join('test_numpy_arrays', 'test_array')
-        minio_oss_operator.upload_numpy_array(bucket_name, upload_test_numpy_path, test_numpy_array)
-        download_test_numpy_array = minio_oss_operator.download_numpy_array(bucket_name, upload_test_numpy_path)
-        for m_array, m_download_array in zip(test_numpy_array, download_test_numpy_array):
-            assert np.sum(m_array - m_download_array) == 0, 'there is different'
+    elif args.backend_type == 'dummy':
+        oss_operator = DummyOSS(
+            None, None, None
+        )
+    else:
+        raise NotImplementedError(f'{args.backend_type} not implement')
+    bucket_name = args.bucket_name
+    oss_operator.create_bucket(bucket_name)
+    img = cv2.imread(args.image_path)
+    resize_img = cv2.resize(img, (128, 128))
+    upload_image_path = os.path.join('test_images', 'image')
+    upload_image_path = oss_operator.upload_image_file(
+        bucket_name, upload_image_path, resize_img, True, 90
+    )
+    image_numpy = oss_operator.download_image_file(bucket_name, upload_image_path)
+    cv2.imwrite('resize_image.png', image_numpy)
+    test_numpy_array = [
+        np.random.randint(3, 5, (3, 3)).astype(np.uint8),
+        np.random.uniform(0, 1, (5, 5))
+    ]
+    upload_test_numpy_path = os.path.join('test_numpy_arrays', 'test_array')
+    oss_operator.upload_numpy_array(bucket_name, upload_test_numpy_path, test_numpy_array)
+    download_test_numpy_array = oss_operator.download_numpy_array(bucket_name, upload_test_numpy_path)
+    for m_array, m_download_array in zip(test_numpy_array, download_test_numpy_array):
+        assert np.sum(m_array - m_download_array) == 0, 'there is different'
