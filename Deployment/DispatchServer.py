d6f879e45749631ab60ca5271344580d43b8fe26
novioleo <74431893@qq.com>
2021-03-17 23:22:05 +0800

    增加ocr接口的配置

--- Deployment/DispatchServer.py
+++ Deployment/DispatchServer.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI
 
 from Deployment.server_config import DEPLOY_VERSION, SERVER_NAME
 from Deployment.DispatchInterfaces.DummyInterface import router as dummy_interface_router
+from Deployment.DispatchInterfaces.OCRRelatedInterface import router as ocr_interface_router
 
 app = FastAPI(title=SERVER_NAME, version=DEPLOY_VERSION)
 
+# dummy_interface是用来测试当前服务器是否成功启动
 available_interfaces = [
-    (dummy_interface_router,'/dummy_interface'),
+    (dummy_interface_router, '/dummy_interface'),
+    (ocr_interface_router, '/ocr_interface'),
 ]
 for m_router, m_path_prefix in available_interfaces:
     app.include_router(m_router, prefix=m_path_prefix)
