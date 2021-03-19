ddd261891779d014d0ad6b37dcd2b31bdf71c96a
novioleo <74431893@qq.com>
2021-03-19 23:48:06 +0800

    simplify the code

--- Operators/ExampleTextRecognizeOperator/TextRecognizeOperator.py
+++ Operators/ExampleTextRecognizeOperator/TextRecognizeOperator.py
@@ -1,23 +1,23 @@
 from abc import ABC
 
-import numpy as np
 import cv2
-from Operators.DummyAlgorithm import DummyAlgorithm
+import numpy as np
+
+from Operators.DummyAlgorithmWithModel import DummyAlgorithmWithModel
 from Operators.ExampleTextRecognizeOperator.CTCDecoder import CTCLabelConverter
 from Utils.GeometryUtils import resize_with_height, center_pad_image_with_specific_base
 from Utils.InferenceHelpers.BaseInferenceHelper import TritonInferenceHelper
 
 
-class TextRecognizeOperator(DummyAlgorithm, ABC):
+class TextRecognizeOperator(DummyAlgorithmWithModel, ABC):
     """
     可以根据自己的需求定制自己的文本识别模型
     """
     name = "文本识别"
     __version__ = 'v1.0.20210315'
 
-    def __init__(self, _inference_helper, _alphabet_config_name, _is_test):
-        super().__init__(_is_test)
-        self.inference_helper = _inference_helper
+    def __init__(self, _inference_config, _alphabet_config_name, _is_test):
+        super().__init__(_inference_config, _is_test)
         self.ctc_decoder = CTCLabelConverter(_alphabet_config_name)
 
 
@@ -27,6 +27,31 @@ class GeneralCRNN(TextRecognizeOperator):
     """
     name = "基于CRNN的自然场景文本识别"
     __version__ = 'v1.0.20210315'
+
+    def get_inference_helper(self):
+        if self.inference_config['name'] == 'triton':
+            support_backbone_type = {'mbv3', 'res34'}
+            backbone_type = 'res34'
+            if 'backbone_type' in self.inference_config and \
+                    self.inference_config['backbone_type'] in support_backbone_type:
+                backbone_type = self.inference_config['backbone_type']
+            else:
+                print(f'crnn use the default backbone:{backbone_type}')
+            inference_helper = TritonInferenceHelper(
+                f'CRNN_{backbone_type}',
+                self.inference_config['triton_url'],
+                self.inference_config['triton_port'],
+                f'CRNN_{backbone_type}',
+                1
+            )
+            inference_helper.add_image_input('INPUT__0', (32, -1, 3), '识别用的图像',
+                                             ([127.5, 127.5, 127.5], [127.5, 127.5, 127.5]))
+            inference_helper.add_output('OUTPUT__0', (-1, 1), '识别的max')
+            inference_helper.add_output('OUTPUT__1', (-1, 1), '识别的argmax的结果')
+            self.inference_helper = inference_helper
+        else:
+            raise NotImplementedError(
+                f"{self.inference_config['name']} helper for crnn text recognize not implement")
 
     def execute(self, _image):
         to_return_result = {
@@ -52,7 +77,6 @@ class GeneralCRNN(TextRecognizeOperator):
 
 
 if __name__ == '__main__':
-    import cv2
     from argparse import ArgumentParser
 
     ag = ArgumentParser('Text Recognize Example')
@@ -61,15 +85,15 @@ if __name__ == '__main__':
     ag.add_argument('-p', '--triton_port', dest='triton_port', type=int, default=8001, help='triton grpc 端口')
     args = ag.parse_args()
     img = cv2.imread(args.image_path)
-    crnn_mbv3 = TritonInferenceHelper('crnn_mbv3', args.triton_url, args.triton_port, 'CRNN_mbv3', 1)
-    crnn_mbv3.add_image_input('INPUT__0', (32, -1, 3), '识别用的图像', ([127.5, 127.5, 127.5], [127.5, 127.5, 127.5]))
-    crnn_mbv3.add_output('OUTPUT__0', (-1, 1), '识别的max')
-    crnn_mbv3.add_output('OUTPUT__1', (-1, 1), '识别的argmax的结果')
-    crnn_res34 = TritonInferenceHelper('crnn_res34', args.triton_url, args.triton_port, 'CRNN_res34', 1)
-    crnn_res34.add_image_input('INPUT__0', (32, -1, 3), '识别用的图像', ([127.5, 127.5, 127.5], [127.5, 127.5, 127.5]))
-    crnn_res34.add_output('OUTPUT__0', (-1, 1), '识别的max')
-    crnn_res34.add_output('OUTPUT__1', (-1, 1), '识别的argmax的结果')
-    crnn_mbv3_handler = GeneralCRNN(crnn_mbv3, 'common', True)
-    crnn_res34_handler = GeneralCRNN(crnn_res34, 'common', True)
+    crnn_mbv3_handler = GeneralCRNN({
+        'name': 'triton',
+        'backbone_type': 'mbv3',
+        'triton_url': args.triton_url,
+        'triton_port': args.triton_port}, 'common', True)
+    crnn_res34_handler = GeneralCRNN({
+        'name': 'triton',
+        'backbone_type': 'res34',
+        'triton_url': args.triton_url,
+        'triton_port': args.triton_port}, 'common', True)
     print(crnn_mbv3_handler.execute(img))
     print(crnn_res34_handler.execute(img))
