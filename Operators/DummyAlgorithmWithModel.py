e57e862e85616d52b9655f49158048aef3078b2e
novioleo <74431893@qq.com>
2021-03-19 23:11:53 +0800

    增加带模型的dummy algorithm的基类，方便进行模型配置

---
+++ Operators/DummyAlgorithmWithModel.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+from Operators.DummyAlgorithm import DummyAlgorithm
+
+
+class DummyAlgorithmWithModel(DummyAlgorithm, ABC):
+
+    def __init__(self, _inference_config, _is_test):
+        super().__init__(_is_test)
+        self.inference_config = _inference_config
+        self.get_inference_helper()
+
+    @abstractmethod
+    def get_inference_helper(self):
+        pass
