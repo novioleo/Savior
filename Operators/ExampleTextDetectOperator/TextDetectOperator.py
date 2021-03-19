1f59eb18a015d78c8b852dff0fd8f6be6b7f9410
novioleo <74431893@qq.com>
2021-03-19 23:39:08 +0800

    简化代码实现

--- Operators/ExampleTextDetectOperator/TextDetectOperator.py
+++ Operators/ExampleTextDetectOperator/TextDetectOperator.py
@@ -1,27 +1,27 @@
 from abc import ABC
 
-from Operators.DummyAlgorithm import DummyAlgorithm
 import cv2
 import numpy as np
 
+from Operators.DummyAlgorithmWithModel import DummyAlgorithmWithModel
 from Operators.ExampleTextDetectOperator.PostProcess import db_post_process
 from Utils.GeometryUtils import resize_with_short_side, resize_with_specific_base
 from Utils.InferenceHelpers.BaseInferenceHelper import TritonInferenceHelper
 
 
-class TextDetectOperator(DummyAlgorithm, ABC):
+class TextDetectOperator(DummyAlgorithmWithModel, ABC):
     name = 'TextDetect'
     __version__ = 'v1.0.20210316'
 
-    def __init__(self, _inference_helper, _is_test):
-        super().__init__(_is_test)
-        self.inference_helper = _inference_helper
+    def __init__(self, _inference_config, _is_test):
+        super().__init__(_inference_config, _is_test)
 
 
 class GeneralDBDetect(TextDetectOperator):
     """
     避免DB名称与数据库重叠，所以起名DBDetect
     """
+
     name = '自然场景下的基于DB的文本检测'
     __version__ = 'v1.0.20210316'
 
@@ -30,6 +30,30 @@ class GeneralDBDetect(TextDetectOperator):
         self.threshold = _threshold
         self.bbox_scale_ratio = _bbox_scale_ratio
         self.shortest_length = _shortest_length
+
+    def get_inference_helper(self):
+        if self.inference_config['name'] == 'triton':
+            support_backbone_type = {'mbv3', 'res18'}
+            backbone_type = 'res18'
+            if 'backbone_type' in self.inference_config and \
+                    self.inference_config['backbone_type'] in support_backbone_type:
+                backbone_type = self.inference_config['backbone_type']
+            else:
+                print(f'db use the default backbone:{backbone_type}')
+            inference_helper = TritonInferenceHelper(
+                f'DB_{backbone_type}',
+                self.inference_config['triton_url'],
+                self.inference_config['triton_port'],
+                f'DB_{backbone_type}',
+                1
+            )
+            inference_helper.add_image_input('INPUT__0', (-1, -1, 3), '识别用的图像',
+                                             ([123.675, 116.28, 103.53], [58.395, 57.12, 57.375]))
+            inference_helper.add_output('OUTPUT__0', (1, -1, -1), 'detect score')
+            self.inference_helper = inference_helper
+        else:
+            raise NotImplementedError(
+                f"{self.inference_config['name']} helper for retina face detect not implement")
 
     def execute(self, _image):
         to_return_result = {
@@ -68,14 +92,20 @@ if __name__ == '__main__':
     ag.add_argument('-p', '--triton_port', dest='triton_port', type=int, default=8001, help='triton grpc 端口')
     args = ag.parse_args()
     img = cv2.imread(args.image_path)
-    db_mbv3 = TritonInferenceHelper('DB_mbv3', args.triton_url, args.triton_port, 'DB_mbv3', 1)
-    db_mbv3.add_image_input('INPUT__0', (-1, -1, 3), '识别用的图像', ([123.675, 116.28, 103.53], [58.395, 57.12, 57.375]))
-    db_mbv3.add_output('OUTPUT__0', (1, -1, -1), 'detect score')
-    db_res18 = TritonInferenceHelper('DB_res18', args.triton_url, args.triton_port, 'DB_res18', 1)
-    db_res18.add_image_input('INPUT__0', (-1, -1, 3), '识别用的图像', ([123.675, 116.28, 103.53], [58.395, 57.12, 57.375]))
-    db_res18.add_output('OUTPUT__0', (1, -1, -1), 'detect score')
-    db_mbv3_handler = GeneralDBDetect(db_mbv3, True, 0.3, 1.5, 5)
-    db_res18_handler = GeneralDBDetect(db_res18, True, 0.3, 5, 5)
+    db_mbv3_handler = GeneralDBDetect({
+        'name': 'triton',
+        'backbone_type': 'mbv3',
+        'triton_url': args.triton_url,
+        'triton_port': args.triton_port},
+        True, 0.3, 1.5, 5
+    )
+    db_res18_handler = GeneralDBDetect({
+        'name': 'triton',
+        'backbone_type': 'res18',
+        'triton_url': args.triton_url,
+        'triton_port': args.triton_port},
+        True, 0.3, 5, 5
+    )
     db_mbv3_boxes = db_mbv3_handler.execute(img)['locations']
     db_res18_boxes = db_res18_handler.execute(img)['locations']
     db_mbv3_result_to_show = img.copy()
