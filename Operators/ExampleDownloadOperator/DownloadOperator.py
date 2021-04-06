d113d6a008093001ebb2e4ec5a121b9727654d0a
Tao Luo <744351893@qq.com>
2021-04-06 11:23:57 +0800

    抽象出来下载算子，并使用PIL替代skimage的io read

--- Operators/ExampleDownloadOperator/DownloadOperator.py
+++ Operators/ExampleDownloadOperator/DownloadOperator.py
@@ -1,57 +1,97 @@
 import os
 import traceback as tb
+from abc import ABC
 from collections import OrderedDict
+from io import BytesIO
 
-from skimage import io
-import cv2
+import requests
+from PIL import Image
 
 from Operators.DummyOperator import DummyOperator
 from Utils.Exceptions import ImageDownloadTimeoutException, ImageFileSizeAbnormalException, CustomException, \
-    ImageFormatNotSupportException, ConsumerAlgorithmUncatchException
+    ImageFormatNotSupportException, ConsumerAlgorithmUncatchException, DownloadURLTimeoutException, \
+    DownloadURLNotAvailableException
 from Utils.Storage import CloudObjectStorage
-from Utils.misc import get_uuid_name, get_date_string
+from Utils.misc import get_uuid_name, get_date_string, convert_pil_to_numpy
 
 
-class ImageDownloadOperator(DummyOperator):
+class DownloadOperator(DummyOperator, ABC):
+    name = '下载'
+    __version__ = 'v1.0.20210406'
+
+    def __init__(self, _is_test, _timeout=30):
+        super().__init__(_is_test)
+        self.timeout = _timeout
+
+    def download_url(self, _to_download_url, _chunk_size=1024 * 1):
+        to_return_bytes = BytesIO()
+        try:
+            download_response = requests.get(_to_download_url, timeout=self.timeout)
+            for m_chuck in download_response.iter_content(chunk_size=_chunk_size):
+                to_return_bytes.write(m_chuck)
+        except requests.exceptions.Timeout as te:
+            raise DownloadURLTimeoutException
+        except requests.exceptions.InvalidURL as ie:
+            raise DownloadURLNotAvailableException
+        return to_return_bytes
+
+
+class ImageDownloadOperator(DownloadOperator):
     """
     将图像从网上下载下来，然后存储在OSS中，方便集群中的worker进行取用。
     """
 
     name = "图像下载"
-    __version__ = 'v1.0.20210312'
+    __version__ = 'v1.0.20210406'
 
-    def __init__(self, _is_test):
-        super().__init__(_is_test)
+    def __init__(self, _is_test, _timeout=30):
+        super().__init__(_is_test, _timeout)
 
     def execute(
-            self, _to_download_url, _oss_helper: CloudObjectStorage,
-            _timeout=30, _image_size_threshold=10,
+            self, _to_download_url, _oss_helper: CloudObjectStorage = None,
+            _image_size_threshold=10,
 
     ):
+        """
+        下载指定url的图像文件
+
+        Args:
+            _to_download_url:   待下载的图像url
+            _oss_helper:    oss helper用于存储下载好的数据，可以为空
+            _image_size_threshold:  图像字节数(KB)限制，如果低于阈值会异常
+
+        Returns:    下载完成的结果
+
+        """
         to_return_result = OrderedDict()
         try:
-            request_image = cv2.cvtColor(io.imread(_to_download_url), cv2.COLOR_RGB2BGR)
+            download_result_io = self.download_url(_to_download_url, 1024 * 2)
+            request_image = Image.open(download_result_io)
             if request_image is None:
                 raise ImageFormatNotSupportException(
-                    f'image:{_to_download_url} format not support,cannot decode by opencv')
-            if len(request_image.shape) == 3:
-                image_h, image_w, image_c = request_image.shape
-            else:
-                image_h, image_w = request_image.shape[:2]
-                image_c = 1
-            if _image_size_threshold is not None and request_image.nbytes < _image_size_threshold * 1024:
+                    f'image:{_to_download_url} format not support,cannot decode by PILLOW')
+            image_c = len(request_image.getbands())
+            image_h = request_image.height
+            image_w = request_image.width
+            request_image_np = convert_pil_to_numpy(request_image)
+            if _image_size_threshold is not None and request_image_np.nbytes < _image_size_threshold * 1024:
                 raise ImageFileSizeAbnormalException(
                     f'image:{_to_download_url} is small than threshold,it may not be a normal picture')
-            file_name = get_uuid_name()
-            oss_path = os.path.join(get_date_string(), file_name)
-            # 存储原始图像
-            saved_path = _oss_helper.upload_image_file('downloaded-image', oss_path, request_image,
-                                                       _enable_compress=False)
+            # 有些情况是不需要存储的，可能直接就用了。
+            if _oss_helper:
+                file_name = get_uuid_name()
+                oss_path = os.path.join(get_date_string(), file_name)
+                # 存储原始图像
+                saved_path = _oss_helper.upload_image_file('downloaded-image', oss_path, request_image,
+                                                           _enable_compress=False)
+            else:
+                saved_path = ''
             to_return_result['bucket_name'] = 'downloaded-image'
             to_return_result['saved_path'] = saved_path
             to_return_result['image_height'] = image_h
             to_return_result['image_width'] = image_w
             to_return_result['image_channel'] = image_c
+            to_return_result['image'] = request_image_np
             return to_return_result
         except TimeoutError as te:
             raise ImageDownloadTimeoutException(f'{_to_download_url} download timeout')
@@ -65,6 +105,7 @@ if __name__ == '__main__':
     from pprint import pprint
     from argparse import ArgumentParser
     from Utils.Storage import DummyOSS
+
     ag = ArgumentParser('Image Download Example')
     ag.add_argument('-i', '--image', type=str, dest='image',
                     default='https://www.baidu.com/img/flexible/logo/pc/result@2.png',
