d69be0ac3fe7e4f68da40896cbd620af6b889885
Tao Luo <744351893@qq.com>
2021-04-08 10:49:22 +0800

    动态import api router。

--- Deployment/DispatchServer.py
+++ Deployment/DispatchServer.py
@@ -1,19 +1,26 @@
 import uvicorn
 from fastapi import FastAPI
-
+import importlib
 from Deployment.server_config import DEPLOY_VERSION, SERVER_NAME
-from Deployment.DispatchInterfaces.DummyInterface import router as dummy_interface_router
-from Deployment.DispatchInterfaces.OCRRelatedInterface import router as ocr_interface_router
 
 app = FastAPI(title=SERVER_NAME, version=DEPLOY_VERSION)
 
 # dummy_interface是用来测试当前服务器是否成功启动
 available_interfaces = [
-    (dummy_interface_router, '/dummy_interface'),
-    (ocr_interface_router, '/ocr_interface'),
+    ('DummyInterface', '/dummy_interface'),
+    ('OCRRelatedInterface', '/ocr_interface'),
 ]
-for m_router, m_path_prefix in available_interfaces:
-    app.include_router(m_router, prefix=m_path_prefix)
+for m_router_package, m_path_prefix in available_interfaces:
+    try:
+        m_interface_module = importlib.import_module(f'Deployment.DispatchInterfaces.{m_router_package}')
+        m_interface_router = getattr(m_interface_module, 'router')
+        app.include_router(m_interface_router, prefix=m_path_prefix)
+        print(f'{m_router_package} load finish')
+    except AttributeError as ae:
+        print(f'the APIRouter must name as router in {m_router_package}')
+    except Exception as e:
+        print(e)
+        print(f'{m_router_package} not found.')
 
 if __name__ == '__main__':
     from Deployment.server_config import DISPATCH_SERVER_PORT
