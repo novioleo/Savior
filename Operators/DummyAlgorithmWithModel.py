7a5a1fc2af1cc2162134a4b7d3b1a6bce3d764dc
Tao Luo <744351893@qq.com>
2021-04-09 18:08:31 +0800

    增加配置默认的inference helper

--- Operators/DummyAlgorithmWithModel.py
+++ Operators/DummyAlgorithmWithModel.py
@@ -8,6 +8,7 @@ class DummyAlgorithmWithModel(DummyAlgorithm, ABC):
     def __init__(self, _inference_config, _is_test):
         super().__init__(_is_test)
         self.inference_config = _inference_config
+        self.inference_helper = None
         self.get_inference_helper()
 
     @abstractmethod
